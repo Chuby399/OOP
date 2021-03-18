648b4f638b9d70e70c528e06277fc57e16c400f5
Chuby399 <80063319+Chuby399@users.noreply.github.com>
2021-03-18 12:20:56 -0400

    Add files via upload

---
+++ compositionExample/src/compositionExample/Console.java
@@ -0,0 +1,12 @@
+package compositionExample;
+
+import java.util.ArrayList;
+
+public class Console {
+	private String Name;
+	private String Manufacture;
+	private String Type;
+	private String Color;
+	private String Year;
+	private ArrayList<Games> GameLibrary;
+}
---
+++ compositionExample/src/compositionExample/Games.java
@@ -0,0 +1,12 @@
+package compositionExample;
+
+public class Games {
+	private String name;
+	private String publisher;
+	private String Year;
+	private String MediaType;
+	private String Genre;
+	private String Status;
+	private String Rating;
+	
+}
